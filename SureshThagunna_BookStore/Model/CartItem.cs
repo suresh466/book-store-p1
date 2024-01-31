aa0a345d155225034bec9892a7c4f803e570f644
Suresh <suresh.thagunna77@gmail.com>
2024-01-31 10:16:16 -0500

    add CartItem and CartItemList to models

---
+++ SureshThagunna_BookStore/Model/CartItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SureshThagunna_BookStore.Model
+{
+    public class CartItem
+    {
+        public CartItem() { }
+
+        public CartItem(Book book, int quantity)
+        {
+            this.Book = book;
+            this.Quantity = quantity;
+        }
+
+        public Book Book { get; set; }
+        public int Quantity { get; set; }
+
+        public void AddQuantity(int quantity)
+        {
+            this.Quantity += quantity;
+        }
+
+        public string Display()
+        {
+            string displayString = string.Format("{0} ({1} at {2})",
+                Book.Name,
+                Quantity.ToString(),
+                Book.UnitPrice.ToString("c")
+            );
+            return displayString;
+        }
+    }
+}
--- SureshThagunna_BookStore/Products.aspx.designer.cs
+++ SureshThagunna_BookStore/Products.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace SureshThagunna_BookStore
         protected global::System.Web.UI.WebControls.DropDownList ddlProducts;
 
         /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+
+        /// <summary>
         /// lblName control.
         /// </summary>
         /// <remarks>
